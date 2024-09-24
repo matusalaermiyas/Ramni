34fbf376c58b9bd9bd51aa9ab9da8bd3b62ed025
MatusalaErmiyas <matusala.dev@gmail.com>
2024-09-24 14:37:32 +0300

    Implement product details

--- lib/product_details/product_details.dart
+++ lib/product_details/product_details.dart
@@ -1,4 +1,9 @@
+import 'package:contained_tab_bar_view/contained_tab_bar_view.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ramniui/styles/colors.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   const ProductDetailsScreen({super.key});
@@ -7,25 +12,38 @@ class ProductDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.arrow_back),
         actions: [
           Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Icon(Icons.favorite),
+            child: Icon(
+              Icons.favorite,
+            ),
           )
         ],
       ),
       body: Column(
         children: [
-          Container(
-            width: 305,
-            height: 168,
-            color: Colors.red,
+          Align(
+            child: Container(
+              margin: const EdgeInsets.only(left: 74),
+              width: 305,
+              height: 168,
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: Image.asset(
+                  "assets/images/product_detail.png",
+                  width: 305,
+                  height: 168,
+                ),
+              ),
+            ),
           ),
           Container(
+            margin: const EdgeInsets.only(top: 41),
             width: 36,
             height: 5,
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
                   width: 5,
@@ -51,8 +69,147 @@ class ProductDetailsScreen extends StatelessWidget {
               ],
             ),
           ),
-          Row(
-            children: [Text("Ramni Chair"), Icon(Icons.star), Text("1700")],
+          SizedBox(
+            height: 37,
+          ),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.only(left: 25, bottom: 30),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 33),
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(right: 17),
+                          child: const Text(
+                            "Ramni Chair",
+                            style: TextStyle(fontSize: 23),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 107),
+                          child: Row(
+                            children: [
+                              SvgPicture.asset("assets/svgs/star.svg"),
+                              SizedBox(
+                                width: 6.5,
+                              ),
+                              Text(
+                                "4.5",
+                                style: TextStyle(fontSize: 15),
+                              )
+                            ],
+                          ),
+                        ),
+                        Text(
+                          "\$1700",
+                          style: TextStyle(
+                              fontSize: 21,
+                              color: textColor,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: ContainedTabBarView(
+                        tabBarProperties: const TabBarProperties(
+                            unselectedLabelColor: Colors.grey,
+                            labelColor: textColor,
+                            indicatorColor: backgroundColor,
+                            labelPadding: EdgeInsets.all(0),
+                            margin: EdgeInsets.all(0),
+                            padding: EdgeInsets.all(0)),
+                        tabs: const [
+                          Text(
+                            "Description",
+                          ),
+                          Text(
+                            "Reviews",
+                          ),
+                          Text(
+                            "Offers",
+                          ),
+                          Text(
+                            "Policy",
+                          ),
+                        ],
+                        views: [
+                          Container(
+                            margin: const EdgeInsets.only(top: 25),
+                            child: const Text(
+                              "Minimalist Styling Is Not About Creating A Cold, Hard, Empty White Box Of A Home. It Is About Using Simple And Natural Forms, And Taking Away Layers Without Losing The Aesthetic Appeal Of The Space. The Focus Is On Shape, With A Furniture And Accessories.",
+                              style: TextStyle(fontSize: 14),
+                            ),
+                          ),
+                          Container(
+                              margin: const EdgeInsets.only(top: 25),
+                              child: const Text("Two")),
+                          Container(
+                              margin: const EdgeInsets.only(top: 25),
+                              child: const Text("Three")),
+                          Container(
+                              margin: const EdgeInsets.only(top: 25),
+                              child: const Text("Four"))
+                        ]),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(16),
+                        width: 120,
+                        height: 58,
+                        decoration: BoxDecoration(
+                            color: backgroundColor,
+                            borderRadius: BorderRadius.circular(75)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SvgPicture.asset("assets/svgs/minus.svg"),
+                            Text(
+                              "1",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Colors.white),
+                            ),
+                            SvgPicture.asset("assets/svgs/plus.svg")
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 29,
+                      ),
+                      Container(
+                        width: 194,
+                        height: 58,
+                        decoration: BoxDecoration(
+                            color: backgroundColor,
+                            borderRadius: BorderRadius.circular(75)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "ADD TO CART",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 17,
+                            ),
+                            SvgPicture.asset("assets/svgs/add_to_cart.svg")
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           )
         ],
       ),
