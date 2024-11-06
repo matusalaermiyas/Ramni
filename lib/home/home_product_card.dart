86a0a3955eafd12776a63a63a988ba085aff1de9
MatusalaErmiyas <matusala.dev@gmail.com>
2024-11-06 21:56:51 +0300

    Add event listener and change the color name

--- lib/home/home_product_card.dart
+++ lib/home/home_product_card.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 import 'package:ramniui/fake_data/trending_products.dart';
+import 'package:ramniui/product_details/product_details.dart';
+import 'package:ramniui/styles/colors.dart';
 
 class HomeProductCard extends StatelessWidget {
   final TrendingProductsModel product;
@@ -9,72 +12,75 @@ class HomeProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 13),
-      child: Card(
-        elevation: 4,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Container(
-          width: 164, // Sets width only for Container
-          height: 222,
-          decoration: BoxDecoration(
-            color: Colors.white,
+    return GestureDetector(
+      onTap: () => Get.to(const ProductDetailsScreen()),
+      child: Container(
+        margin: const EdgeInsets.only(right: 13),
+        child: Card(
+          elevation: 4,
+          shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                margin: const EdgeInsets.all(4),
-                width: 156,
-                height: 128,
-                decoration: BoxDecoration(
-                    color: const Color(0xFFFBFBFB),
-                    borderRadius: BorderRadius.circular(9)),
-                child: Image.asset(
-                  "assets/images/${product.imageFileName}",
-                  width: 136,
-                  height: 103,
-                ),
-              ),
-              const SizedBox(
-                height: 17,
-              ),
-              Text(
-                product.productName,
-                style: const TextStyle(fontSize: 14),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 22.0),
-                    child: Text(
-                      "\$${product.price}",
-                      style: const TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w600,
-                          color: Color(0xFF8B98B4)),
-                    ),
+          child: Container(
+            width: 164, // Sets width only for Container
+            height: 222,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(4),
+                  width: 156,
+                  height: 128,
+                  decoration: BoxDecoration(
+                      color: softBackgroundColor,
+                      borderRadius: BorderRadius.circular(9)),
+                  child: Image.asset(
+                    "assets/images/${product.imageFileName}",
+                    width: 136,
+                    height: 103,
                   ),
-                  Container(
-                    width: 34,
-                    height: 34,
-                    margin: const EdgeInsets.only(right: 13),
-                    child: SvgPicture.asset(
-                      "assets/svgs/add_to_cart.svg",
-                      width: 13,
-                      height: 12,
+                ),
+                const SizedBox(
+                  height: 17,
+                ),
+                Text(
+                  product.productName,
+                  style: const TextStyle(fontSize: 14),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 22.0),
+                      child: Text(
+                        "\$${product.price}",
+                        style: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xFF8B98B4)),
+                      ),
                     ),
-                  )
-                ],
-              )
-            ],
+                    Container(
+                      width: 34,
+                      height: 34,
+                      margin: const EdgeInsets.only(right: 13),
+                      child: SvgPicture.asset(
+                        "assets/svgs/add_to_cart.svg",
+                        width: 13,
+                        height: 12,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
