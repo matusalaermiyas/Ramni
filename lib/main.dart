ffdc38ec838623aa32aa98694522aa796ad55b6f
MatusalaErmiyas <matusala.dev@gmail.com>
2024-11-07 10:00:20 +0300

    Place the theme in separate file and use it

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ramniui/landing/landing_screen.dart';
+import 'package:ramniui/styles/theme.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const LandingScreen(),
-      theme: ThemeData(fontFamily: "SFPro"),
+      theme: theme,
     ),
   );
 }
---
+++ lib/styles/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:ramniui/styles/colors.dart';
+
+final ThemeData theme = ThemeData(
+    fontFamily: "SFPro",
+    appBarTheme: const AppBarTheme(
+        titleTextStyle: TextStyle(color: appbarTextColor, fontSize: 18)));
